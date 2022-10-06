0edb653ed765abcbb2c0de3e11cfd64ad86df5fe
dewasanta2 <dewasanta2@gmail.com>
2022-10-06 17:26:57 +0800

    add mysql connection

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+// setup global config
+dotenv.config();
 
 const router = express.Router();
+
+//create connection to database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+connection.connect();
 
 router
   .route("/login")
@@ -8,21 +22,49 @@ router
     res.render("login");
   })
   .post(function (req, res) {
-    res.send(req.body);
-  })
-  .put(function (req, res) {
-    res.send("puted");
+    const user = req.body.username;
+    connection.query(
+      "SELECT * FROM user WHERE username = ?",
+      [user],
+      (error, results) => {
+        if (results.length > 0) {
+          console.log("get username");
+          if (req.body.password === results[0].password) {
+            res.redirect("/dashboard");
+          } else {
+            console.log("wrong password");
+            res.render("login", { passwordError: "Password salah" });
+          }
+        } else {
+          res.render("login", {
+            userError: "username atau email tidak terdaftar",
+          });
+        }
+      }
+    );
   });
 
+//   add new user
 router
   .route("/register")
   .get(function (req, res) {
     res.render("register");
   })
   .post(function (req, res) {
-    res.send(req.body);
-  })
-  .put(function (req, res) {
-    res.send("puted");
+    let data = req.body;
+
+    // check password and repeat password
+    if (data["password"] != data["repeatPassword"]) {
+      res.render("register", { passwordError: "Password tidak sama" });
+      return;
+    }
+    delete data.repeatPassword;
+    let sql = "INSERT INTO user SET ?";
+    connection.query(sql, [data], (error, results) => {
+      if (error) throw error;
+      console.log("new user added");
+      res.redirect("/dashboard");
+    });
   });
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,4 @@
 const express = require("express");
-const mysql = require("mysql");
-const dotenv = require("dotenv");
-
-// setup global config
-dotenv.config();
-
-//create connection to database
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
-connection.connect();
 
 //set up express
 const app = express();
@@ -22,6 +8,10 @@ app.set("view engine", "ejs");
 
 // route
 app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/dashboard", function (req, res) {
   res.render("dashboard");
 });
 
