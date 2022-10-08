6a6a9b0a3266d58f02f9ff1ee487cd958f2b2ec6
dewasanta2 <dewasanta2@gmail.com>
2022-10-08 11:31:28 +0800

    add auth system and database

--- routes/auth.js
+++ routes/auth.js
@@ -27,24 +27,26 @@ router
       "SELECT * FROM user WHERE username = ?",
       [user],
       (error, results) => {
-        if (results.length > 0) {
-          console.log("get username");
-          if (req.body.password === results[0].password) {
-            res.redirect("/dashboard");
-          } else {
-            console.log("wrong password");
-            res.render("login", { passwordError: "Password salah" });
-          }
-        } else {
+        if (results == 0) {
           res.render("login", {
-            userError: "username atau email tidak terdaftar",
+            userError: "username tidak terdaftar",
           });
+          return;
         }
+        if (req.body.password != results[0].password) {
+          console.log("wrong password");
+          res.render("login", { passwordError: "Password salah" });
+          return;
+        }
+
+        req.session.username = results[0].username;
+        res.redirect("/dashboard");
       }
     );
   });
 
-//   add new user
+// add new user
+// TODO: ADD PASSWORD ENCRYPTION AND MAKE DB FOR IOT DEVICE
 router
   .route("/register")
   .get(function (req, res) {
@@ -53,7 +55,7 @@ router
   .post(function (req, res) {
     let data = req.body;
 
-    // check password and repeat password
+    // check password and repeated password
     if (data["password"] != data["repeatPassword"]) {
       res.render("register", { passwordError: "Password tidak sama" });
       return;
@@ -62,9 +64,15 @@ router
     let sql = "INSERT INTO user SET ?";
     connection.query(sql, [data], (error, results) => {
       if (error) throw error;
+      req.session.username = results[0].username;
       console.log("new user added");
       res.redirect("/dashboard");
     });
   });
+
+router.get("/logout", function (req, res) {
+  req.session.destroy();
+  res.redirect("login");
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const sessions = require("express-session");
+const dotenv = require("dotenv");
+
+// setup global config
+dotenv.config();
 
 //set up express
 const app = express();
@@ -6,18 +11,42 @@ app.use(express.static("views"));
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
+// setup session
+app.use(
+  sessions({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    cookie: { maxAge: 86400000 }, // expired in one day
+    resave: false,
+  })
+);
+
+// user login checker
+function userCheck(req, res, next) {
+  if (req.session.username) {
+    next();
+  } else {
+    res.redirect("login");
+    return;
+  }
+}
+
 // route
 app.get("/", function (req, res) {
-  res.render("index");
+  if (req.session.username) {
+    res.redirect("dashboard");
+  } else {
+    res.redirect("login");
+  }
 });
 
-app.get("/dashboard", function (req, res) {
-  res.render("dashboard");
+app.get("/dashboard", userCheck, function (req, res) {
+  res.render("dashboard", { userData: req.session });
 });
 
 authRouter = require("./routes/auth");
 app.use(authRouter);
 
-app.listen(process.env.PORT, "0.0.0.0");
-
-console.log(`server started, visit http://localhost:${process.env.PORT}`);
+app.listen(process.env.PORT, "0.0.0.0", function () {
+  console.log(`server started, visit http://localhost:${process.env.PORT}`);
+});
